65268fc9b85ca528aae7fb2aab5f79806895b4c9
Ignasi Barrera <nacx@apache.org>
2021-12-09 18:50:22 +0100

    linter

--- example_test.go
+++ example_test.go
@@ -17,6 +17,7 @@ package log
 import (
 	"context"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/tetratelabs/telemetry"
@@ -27,6 +28,9 @@ func mockTime() time.Time {
 }
 
 func ExampleLogger() {
+
+	fmt.Println("--- structured logging ---")
+
 	logger := Register("mylogger", "Custom logger")
 	logger.now = mockTime // Mock time to have a consistent output
 
@@ -43,14 +47,8 @@ func ExampleLogger() {
 	ctx := telemetry.KeyValuesToContext(context.Background(), "request-id", 123)
 	logger.Context(ctx).With("component", "middleware").Info("enriched message")
 
-	// Output:
-	// time="2021/12/09 17:37:46" level=info scope="mylogger" msg="an info message with values" key="value"
-	// time="2021/12/09 17:37:46" level=error scope="mylogger" msg="validation error" arg1="invalid-value" error="validation failed"
-	// time="2021/12/09 17:37:46" level=debug scope="mylogger" msg="an enabled debug message"
-	// time="2021/12/09 17:37:46" level=info scope="mylogger" msg="enriched message" request-id=123 component="middleware"
-}
+	fmt.Println("--- unstructured logging ---")
 
-func ExampleUnstructured() {
 	unstructured := RegisterUnstructured("unstructured", "Unstructured logger")
 	unstructured.now = mockTime // Mock time to have a consistent output
 
@@ -64,10 +62,16 @@ func ExampleUnstructured() {
 	unstructured.Debug("an enabled debug message")
 
 	// Propagating values
-	ctx := telemetry.KeyValuesToContext(context.Background(), "request-id", 123)
+	ctx = telemetry.KeyValuesToContext(context.Background(), "request-id", 123)
 	unstructured.Context(ctx).With("component", "middleware").Info("enriched message")
 
 	// Output:
+	// --- structured logging ---
+	// time="2021/12/09 17:37:46" level=info scope="mylogger" msg="an info message with values" key="value"
+	// time="2021/12/09 17:37:46" level=error scope="mylogger" msg="validation error" arg1="invalid-value" error="validation failed"
+	// time="2021/12/09 17:37:46" level=debug scope="mylogger" msg="an enabled debug message"
+	// time="2021/12/09 17:37:46" level=info scope="mylogger" msg="enriched message" request-id=123 component="middleware"
+	// --- unstructured logging ---
 	// 2021/12/09 17:37:46  info 	unstructured	an info message with a value
 	// 2021/12/09 17:37:46  error	unstructured	validation error in arg1: validation failed
 	// 2021/12/09 17:37:46  debug	unstructured	an enabled debug message
